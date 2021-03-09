f171c68047d74da9b6e898174117f709d8e7ea1a
kunalkale98 <kunal,k2898@gmail.com>
2021-03-10 00:22:46 +0530

    Added: Implementing Generic class to find maximum

--- src/main/java/com/bridgelabz/FindMaximum.java
+++ src/main/java/com/bridgelabz/FindMaximum.java
@@ -1,7 +1,17 @@
 package com.bridgelabz;
 
-public class FindMaximum {
-    public static <T extends Comparable> T giveMaximum(T input1, T input2, T input3) {
+public class FindMaximum <T extends Comparable> {
+    public T input1;
+    public T input2;
+    public T input3;
+
+    public FindMaximum(T input1,T input2,T input3){
+        this.input1 = input1;
+        this.input2 = input2;
+        this.input3 = input3;
+    }
+
+    public T giveMaximum() {
         if (input1.compareTo(input2) > 0) {
             if (input1.compareTo(input3) > 0) {
                 return input1;
@@ -15,4 +25,5 @@ public class FindMaximum {
         }
     }
 }
+
 
--- src/test/java/com/bridgelabz/FindMaximumTest.java
+++ src/test/java/com/bridgelabz/FindMaximumTest.java
@@ -7,73 +7,73 @@ public class FindMaximumTest {
 
     @Test
     public void given_num1_greater_should_return_num1() {
-        FindMaximum findMax = new FindMaximum();
-        int maxNo = findMax.giveMaximum(8,5,6);
+        FindMaximum findMax = new FindMaximum(8,5,6);
+        int maxNo = (int) findMax.giveMaximum();
         Assert.assertEquals(8,maxNo);
     }
 
     @Test
     public void given_num2_greater_should_return_num2() {
-        FindMaximum findMax = new FindMaximum();
-        int maxNo = findMax.giveMaximum(6,8,5);
+        FindMaximum findMax = new FindMaximum(6,8,5);
+        int maxNo = (int) findMax.giveMaximum();
         Assert.assertEquals(8,maxNo);
     }
 
     @Test
     public void given_num3_greater_should_return_num3() {
-        FindMaximum findMax = new FindMaximum();
-        int maxNo = findMax.giveMaximum(5,6,8);
+        FindMaximum findMax = new FindMaximum(5,6,8);
+        int maxNo = (int) findMax.giveMaximum();
         Assert.assertEquals(8,maxNo);
     }
 
     @Test
     public void given_float1_greater_should_return_float1() {
-        FindMaximum findMax = new FindMaximum();
         Float num1 = (float) 8.5;
         Float num2 = (float) 5.6;
         Float num3 = (float) 6.8;
-        Float maxNo = findMax.giveMaximum(num1,num2,num3);
+        FindMaximum findMax = new FindMaximum(num1,num2,num3);
+        Float maxNo = (Float) findMax.giveMaximum();
         Assert.assertEquals(num1,maxNo);
     }
 
     @Test
     public void given_float2_greater_should_return_float2() {
-        FindMaximum findMax = new FindMaximum();
         Float num1 = (float) 6.8;
         Float num2 = (float) 8.5;
         Float num3 = (float) 5.6;
-        Float maxNo = findMax.giveMaximum(num1,num2,num3);
+        FindMaximum findMax = new FindMaximum(num1,num2,num3);
+        Float maxNo = (Float) findMax.giveMaximum();
         Assert.assertEquals(num2,maxNo);
     }
 
     @Test
     public void given_float3_greater_should_return_float3() {
-        FindMaximum findMax = new FindMaximum();
         Float num1 = (float) 5.6;
         Float num2 = (float) 6.8;
         Float num3 = (float) 8.5;
-        Float maxNo = findMax.giveMaximum(num1,num2,num3);
+        FindMaximum findMax = new FindMaximum(num1,num2,num3);
+        Float maxNo = (Float) findMax.giveMaximum();
         Assert.assertEquals(num3,maxNo);
     }
 
     @Test
     public void given_string1_greater_should_return_string1() {
-        FindMaximum findMax = new FindMaximum();
-        String maxNo = findMax.giveMaximum("Peach","Apple","Banana");
+        FindMaximum findMax = new FindMaximum("Peach","Apple","Banana");
+        String maxNo = (String) findMax.giveMaximum();
         Assert.assertEquals("Peach",maxNo);
     }
 
     @Test
     public void given_string2_greater_should_return_string2() {
-        FindMaximum findMax = new FindMaximum();
-        String maxNo = findMax.giveMaximum("Apple","Peach","Banana");
+        FindMaximum findMax = new FindMaximum("Apple","Peach","Banana");
+        String maxNo = (String) findMax.giveMaximum();
         Assert.assertEquals("Peach",maxNo);
     }
 
     @Test
     public void given_string3_greater_should_return_string3() {
-        FindMaximum findMax = new FindMaximum();
-        String maxNo = findMax.giveMaximum("Banana","Apple","Peach");
+        FindMaximum findMax = new FindMaximum("Banana","Apple","Peach");
+        String maxNo = (String) findMax.giveMaximum();
         Assert.assertEquals("Peach",maxNo);
     }
 }
